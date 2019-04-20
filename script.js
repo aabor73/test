616f2257ace02eb39c2996d1d2ca18feea5e363d
Aleksey <aaboryar@yandex.ru>
2019-04-20 17:26:51 +0300

    Это наш первый комит

---
+++ script.js
@@ -0,0 +1,17 @@
+// ОБЪЕКТЫ = скобки {}
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+}
+
+console.log(persone.name);
+
+// МАССИВЫ = скобки []
+let arr = ['plumb.png', 'apple.png', 'orange.png'];
+
+console.log(arr[2]);
+
+// alert("Hello World!");
+
+confirm("Are your here?");
